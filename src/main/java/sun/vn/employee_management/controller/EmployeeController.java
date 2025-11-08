394cb4a7a761e852784d6f984cc16af3ed4335b0
thanh <thanhadp2402@gmail.com>
2025-11-08 23:16:33 +0700

    module 3 Rest api co ban

--- src/main/java/sun/vn/employee_management/controller/EmployeeController.java
+++ src/main/java/sun/vn/employee_management/controller/EmployeeController.java
@@ -3,12 +3,12 @@ package sun.vn.employee_management.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import sun.vn.employee_management.entity.Employee;
 import sun.vn.employee_management.service.UtilityService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
@@ -19,11 +19,18 @@ public class EmployeeController {
     @Autowired
     private UtilityService utilityService;
 
+    private final List<Employee> employees = new ArrayList<>();
+
     @PostMapping
     public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
         employee.setId(utilityService.generateEmployeeId());
         employee.setPassword(passwordEncoder.encode(employee.getPassword()));
-
+        employees.add(employee);
         return ResponseEntity.ok(employee);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employees);
     }
 }
